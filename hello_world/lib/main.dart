4d95616d00f35f367f58e60c186750f1701141d9
João Pedro Panza Mainieri <joaopedro.mainieri@gmail.com>
2026-02-20 09:26:50 -0300

    Tradução dos comentários do main.dart

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -7,13 +7,13 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  // Este widget é a raiz da sua aplicação.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
+        // Este é o tema da sua aplicação.
         //
         // TRY THIS: Try running your application with "flutter run". You'll see
         // the application has a purple toolbar. Then, without quitting the app,
